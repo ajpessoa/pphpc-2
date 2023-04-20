78c3e3660e64e8a01aa0aaf456d849bbbeacb3bb
ajpessoa <rvblack90@gmail.com>
2023-04-20 13:18:24 +0100

    Add scripts to run simulations for each algorithm

---
+++ java/BETTERXOR.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+configurations=("config25v1" "config25v2" "config50v1" "config50v2" "config200v1" "config200v2"  "config400v1" "config400v2" "config800v1" "config800v2" "config1600v1" "config1600v2" "config3200v1" "config3200v2" "config6400v1" "config6400v2" "config12800v1" "config12800v2")
+thread_counts=(8 12 16 24 32 64)
+runs=30
+
+mkdir BETTERXOR-Results
+
+for configuration in "${configurations[@]}"
+  do
+    for threads in "${thread_counts[@]}"
+    do
+      echo "Running BETTERXOR with configuration $configuration and $threads threads"
+      for (( i=1; i<=$runs; i++ ))
+      do
+        name="BETTERXOR_$configuration-$threads-$i.txt"
+        sh ./pp.sh -p ../configs/$configuration.txt -ps EX -n $threads -g BETTERXOR -s BETTERXOR-Results/$name
+      done
+    done
+  done
